ba3cce579797cf88d5f20948c202b14d3595013f
Petre Tiberiu <36566884+Petrickah@users.noreply.github.com>
2026-02-20 20:48:56 +0200

    new: added a small compiler

--- app/src/engine/GenesisEngine.ts
+++ app/src/engine/GenesisEngine.ts
@@ -1,5 +1,5 @@
 import parser from '../grammar/grammar.cjs';
-import { KrakoanNodeSchema, type KrakoanNode, type KrakoanProgram } from '../schema/krakoa.schema.js';
+import { KrakoanNodeSchema, KrakoanProgramSchema, type KrakoanInstruction, type KrakoanNode, type KrakoanProgram } from '../schema/krakoa.schema.js';
 
 export interface AgentState {
   name: string;
@@ -8,9 +8,73 @@ export interface AgentState {
   inventory: string[];
 }
 
-export function k(strings: TemplateStringsArray, ...values: any[]): any {
+export function k(strings: TemplateStringsArray, ...values: any[]): KrakoanProgram {
   const raw = strings.reduce((acc, str, i) => acc + str + (values[i] || ""), "");
   const ast = parser.parse(raw);
   
-  return KrakoanNodeSchema.array().parse(ast);
+  // return KrakoanNodeSchema.array().parse(ast);
+  return compile(KrakoanNodeSchema.array().parse(ast));
+}
+
+function compile(nodes: KrakoanNode[]): KrakoanProgram {
+  let instructions: KrakoanInstruction[] = [];
+  let firstTriggerIndex = -1;
+
+  function process(bodyNodes: Record<string, any>[], returnIndex?: number): void {
+    bodyNodes.forEach((activeNode, index) => {
+      const currentIndex = instructions.length;
+      const isLastInstruction = index === bodyNodes.length - 1;
+
+      let defaultNext: number = isLastInstruction ? (returnIndex ?? -1) : currentIndex + 1;
+
+      if (activeNode.type === ':trigger' && !returnIndex && firstTriggerIndex === -1) {
+        firstTriggerIndex = currentIndex;
+      }
+
+      const currInstruction = {
+        type: activeNode.type,
+        params: activeNode.params,
+        next: [defaultNext],
+      }
+
+      instructions.push(currInstruction);
+
+      if (activeNode.body && activeNode.body.length > 0) {
+        const bodyStartIndex = instructions.length;
+
+        if (activeNode.type === ':trigger') {
+          process(activeNode.body, currentIndex); 
+          const exitIndex = instructions.length;
+          currInstruction.next = [bodyStartIndex, exitIndex];
+        } 
+        else {
+          process(activeNode.body, defaultNext);
+          
+          if (activeNode.type === ':anchor') {
+            const exitIndex = instructions.length;
+            currInstruction.next = [bodyStartIndex, exitIndex];
+          } else {
+            currInstruction.next = [bodyStartIndex];
+          }
+        }
+      } else {
+        currInstruction.next = [defaultNext];
+      }
+
+      if (isLastInstruction) {
+        defaultNext = instructions.length;
+        // Și trebuie să actualizăm manual inst.next dacă nu a fost setat de body
+        if (!activeNode.body || activeNode.body.length === 0) {
+          currInstruction.next = [instructions.length];
+        }
+      }
+    });
+  }
+
+  process(nodes);
+
+  return KrakoanProgramSchema.parse({
+    code: { ...instructions },
+    entry: firstTriggerIndex !== -1 ? firstTriggerIndex : 0,
+  });
 }
--- app/src/schema/krakoa.schema.ts
+++ app/src/schema/krakoa.schema.ts
@@ -1,34 +1,20 @@
 import { z } from "zod";
 
-const ReferenceSchema = z.string().startsWith("@").transform((val) => {
+const ReferenceSchema = z.union([z.string().startsWith("@"), z.string().startsWith("#")]).transform((val) => {
   const cleanPath = val.slice(1);
   const segments = cleanPath.split("::");
   return {
-    kind: "reference",
     original: val,
     segments: segments,
     root: segments[0],
     target: segments[segments.length - 1]
   };
 });
-
-const TagSchema = z.string().startsWith("#").transform((val) => {
-  const cleanPath = val.slice(1);
-  const segments = cleanPath.split("::");
-  return {
-    kind: "tag",
-    original: val,
-    segments: segments,
-    root: segments[0],
-    target: segments[segments.length - 1]
-  };
-});
-
 const BaseKrakoanNodeSchema = z.object({
   type: z.string(),
   params: z.object({
     id: z.string().optional(),
-    tags: z.array(z.union([TagSchema, ReferenceSchema])).default([]),
+    tags: z.array(ReferenceSchema).default([]),
     timestamp: z.number().default(Date.now()),
     code: z.string().optional(),
     isComplex: z.boolean().optional(),
@@ -41,20 +27,18 @@ export const KrakoanNodeSchema = BaseKrakoanNodeSchema.extend({
 
 export type KrakoanNode = z.infer<typeof KrakoanNodeSchema>;
 
-const InstructionSchema = z.object({
+const KrakoanInstructionSchema = z.object({
   type: z.string(),
   params: z.record(z.string(), z.any()).default({}),
-  tags: z.array(z.string()).nullable().default(null),
-  next: z.union([
-    z.number(), 
-    z.object({ then: z.number(), else: z.number() }) // <--- CALEA DE IEȘIRE
-  ])
+  next: z.array(z.number())
 });
 
-const KrakoanProgramSchema = z.object({
-  entry: z.union([z.number(), z.array(z.number())]),
-  text: z.record(z.number(), z.any()).default({}),
-  code: z.record(z.number(), InstructionSchema)
+export const KrakoanProgramSchema = z.object({
+  entry: z.number(),
+  symbols: z.record(z.string(), z.number()).optional().default({}),
+  text: z.record(z.number(), z.any()).optional().default({}),
+  code: z.record(z.number(), KrakoanInstructionSchema)
 }).nullable();
 
 export type KrakoanProgram = z.infer<typeof KrakoanProgramSchema>;
+export type KrakoanInstruction = z.infer<typeof KrakoanInstructionSchema>;
